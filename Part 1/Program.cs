0823adaa1ecfb13925cbe5a096a28167193f546f
Savvas-S <savvas_savva@icloud.com>
2024-02-27 11:02:12 +0200

    Part1_Completed

--- Part 1/Program.cs
+++ Part 1/Program.cs
@@ -7,7 +7,37 @@ namespace Challenge1
         static void Main(string[] args)
         {
             // 1: What does Unreadable do?
+            // Unreadable removes a specific input element from an input array, if element is present
+            // It takes as parameters an array and an element string.
+            // in the function it creates a new array with 1 index less than the input array and sets a flag to Flase
+            // It checks if the element which is provided to the function is present in the array provided
+            // If it is not present then it copies the index of input array to the new array created within the function
+            // If it is present the it sets the Flag as True it skips that specific index. At the end it returns the new array
+
             // 2: Refactor it for better readability
+            Unreadable unreadable = new Unreadable(); // create a new instance of Unreadable class so we can access the function Do
+
+            // Testing data, the original array as input to the function and the element we want to remove
+            string[] originalArray = { "car", "bus", "bike", "train", "car", "airplane" };
+            string elementToRemove = "car";
+
+            // Display original array
+            Console.WriteLine("Original Array:");
+            foreach (string item in originalArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Call Do method to remove the element of the original Array
+            unreadable.Do(elementToRemove, ref originalArray);
+
+            // Display modified array
+            Console.WriteLine("\nArray after removing '" + elementToRemove + "':");
+            foreach (string item in originalArray)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadLine();
         }
     }
--- Part 1/Unreadable.cs
+++ Part 1/Unreadable.cs
@@ -3,31 +3,26 @@ using System.Linq;
 
 namespace Challenge1
 {
-
     public class Unreadable
     {
         public void Do(string element, ref string[] array)
         {
-            // Parameter
-            string x = element;
-            string[] a = array;
+            string[] initialArray = array; //initialize a initialArray as our original Array
+            string[] newArray = new string[array.Length - 1]; //create a copy of the original Array with lenght of 1 index less than the original
+            int index = 0; //will be used to assign the elemnts to the new array 
 
-            // Logic
-            string[] b = new string[a.Length - 1];
-            bool flag = false;
-            for (int i = 0; i < a.Length; i++)
+            foreach (string item in initialArray) //iterate through each index of the array
             {
-                if (flag)
-                    b[i - 1] = a[i];
-                else
+                if (item == element) //condition to check if the index is the element we are looking for
                 {
-                    flag = a[i] == x;
-
-                    if (!flag)
-                        b[i] = a[i];
+                    //if the condition is true then we do nothing and skip the current index
+                    continue;
                 }
+                //otherwise, if the condition is false we assign the index of the original array to the new array 
+                newArray[index++] = item;
             }
-            array = b;
+            Array.Resize(ref newArray, index); //Resizing the new array to remove any null indexes
+            array = newArray; //assiging the new array values to the reference "original" array 
         }
     }
 }
